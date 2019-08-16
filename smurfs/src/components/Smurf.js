98c63b9d184a20d66b599d23bd636b07a2085441
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-16 11:00:47 -0700

    starting delete stretch

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -6,6 +6,7 @@ const Smurf = (props) => {
             <h3>{props.name}</h3>
             <p>Age: {props.age}</p>
             <p>Height: {props.height}</p>
+            <button>Remove Smurf</button>
         </div>
     )
 }
